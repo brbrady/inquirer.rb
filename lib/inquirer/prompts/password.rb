0299946f28d7eece41830c3fc19bc5ea7d798c33
Sergey Kuchmistov <sergey.kuchmistov@gmail.com>
2015-09-04 16:15:35 +0300

    Made Password a subclass of Input

--- lib/inquirer/prompts/password.rb
+++ lib/inquirer/prompts/password.rb
@@ -40,7 +40,7 @@ class PasswordResponseDefault
   end
 end
 
-class Password
+class Password < Input
   def initialize question = nil, renderer = nil, responseRenderer = nil
     @question = question
     @value = ""
@@ -50,74 +50,8 @@ class Password
     @responseRenderer = responseRenderer = PasswordResponseDefault.new()
   end
 
-  def update_prompt
-    # call the renderer
-    @prompt = @renderer.render(@question, @value)
-  end
-
-  def update_response
-    @prompt = @responseRenderer.renderResponse(@question, @value)
-  end
-
-  def update_cursor
-    print IOHelper.char_left * @pos
-  end
-
-  # Run the list selection, wait for the user to select an item and return
-  # the selected index
-  # Params:
-  # +clear+:: +Bool+ whether to clear the selection prompt once this is done
-  #   defaults to true; set it to false if you want the prompt to remain after
-  #   the user is done with selecting
-  # +response+:: +Bool+ whether show the rendered response when this is done
-  #   defaults to true; set it to false if you want the prompt to remain after
-  #   the user is done with selecting
-  def run clear, response
-    # render the
-    IOHelper.render( update_prompt )
-    # loop through user input
-    # IOHelper.read_char
-    IOHelper.read_key_while true do |key|
-      raw  = IOHelper.char_to_raw(key)
-
-      case raw
-      when "backspace"
-        @value = @value.chop
-        IOHelper.rerender( update_prompt )
-        update_cursor
-      when "left"
-        if @pos < @value.length
-          @pos = @pos + 1
-          print IOHelper.char_left
-        end
-      when "right"
-        if @pos > 0
-          @pos = @pos - 1
-          print IOHelper.char_right
-        end
-      when "return"
-      else
-        unless ["up", "down"].include?(raw)
-          @value = @value.insert(@value.length - @pos, key)
-          IOHelper.rerender( update_prompt )
-          update_cursor
-        end
-      end
-      raw != "return"
-    end
-    # clear the final prompt and the line
-    IOHelper.clear if clear
-
-    # show the answer
-    IOHelper.render( update_response ) if response
-
-    # return the value
-    @value
-  end
-
   def self.ask question = nil, opts = {}
     l = Password.new question, opts[:renderer], opts[:rendererResponse]
     l.run opts.fetch(:clear, true), opts.fetch(:response, true)
   end
-
 end
