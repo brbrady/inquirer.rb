93641efbb7cec1d91821a1b0b564c6074b10656e
Ben Brady <bbrady@edgecast.com>
2015-11-30 13:47:36 -0800

    Add choice prompt

--- lib/inquirer.rb
+++ lib/inquirer.rb
@@ -4,6 +4,7 @@ require 'inquirer/prompts/list'
 require 'inquirer/prompts/checkbox'
 require 'inquirer/prompts/input'
 require 'inquirer/prompts/confirm'
+require 'inquirer/prompts/choice'
 
 module Ask
   extend self
@@ -14,10 +15,13 @@ module Ask
   def checkbox *args
     Checkbox.ask *args
   end
-  def input *args
-    Input.ask *args
+  def input *args, **kwargs
+    Input.ask *args, **kwargs
   end
   def confirm *args
     Confirm.ask *args
+  end
+  def choice *args
+    Choice.ask *args
   end
 end
