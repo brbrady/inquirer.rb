53712b1c16d1a6be7d1b66d1d8aae93960434e58
Ben Brady <bbrady@edgecast.com>
2015-11-30 16:03:35 -0800

    Test Ask methods

--- test/classes/input_spec.rb
+++ test/classes/input_spec.rb
@@ -7,23 +7,24 @@ describe Input do
     IOHelper.keys = ['t','y','p','e','d',' ','i','n','p','u','t',"\r"]
   end
 
-  it "should use chars value from the user" do
-    Input.ask("please type input").must_equal "typed input"
-  end
+  [
+    Input.method(:ask),
+    Ask.method(:input)
+  ].each do |meth|
 
-  it "accepts and renders response correctly" do
-    Input.ask("please type input")
-    IOHelper.output.must_equal "please type input: \e[36mtyped input\e[0m\n"
-  end
+    it "should use chars value from the user" do
+      meth.call("please type input").must_equal "typed input"
+    end
 
-  it "should return default value if given and there is no input" do
-    IOHelper.keys = "\r"
-    Confirm.ask("Are you sure?", default: "I'm default").must_equal "I'm default"
-  end
+    it "accepts and renders response correctly" do
+      meth.call("please type input")
+      IOHelper.output.must_equal "please type input: \e[36mtyped input\e[0m\n"
+    end
 
-  it "should provide a password input without displaying the value" do
-    Input.ask("please type password", password: true)
-    IOHelper.output.must_equal "please type password: \e[36m***********\e[0m\n"
+    it "should provide a password input without displaying the value" do
+      meth.call("please type password", password: true)
+      IOHelper.output.must_equal "please type password: \e[36m***********\e[0m\n"
+    end
   end
 
 end
